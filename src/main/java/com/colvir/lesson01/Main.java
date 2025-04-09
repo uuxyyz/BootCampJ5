c01639265d2b8648a16caccbf1b11c3f671b21b1
sdoroshin <sdoroshin@colvir.ru>
2025-04-09 17:09:28 +0300

    BootCampJ5_01 fix at review

--- src/main/java/com/colvir/lesson01/Main.java
+++ src/main/java/com/colvir/lesson01/Main.java
@@ -1,28 +1,27 @@
 package com.colvir.lesson01;
 
-import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        String data = getInputData(System.out, System.in);
+        String data = getInputData();
 
-        System.out.println(String.format("Будет обработана строка %s", data));
+        System.out.printf("Будет обработана строка %s%n", data);
 
-        Arrays.stream(data.toLowerCase().split("[^a-zа-я0-9]")).filter(s -> !s.equals("")).
-                collect(Collectors.toMap(k -> k, u -> 1, (e, r) -> e + 1)).
-                entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).
-                forEach(e -> System.out.println(e.getKey()));
+        Arrays.stream(data.toLowerCase().split("[^a-zа-я0-9]")).filter(s -> !s.isEmpty())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting())).entrySet()
+                .stream().sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .map(Map.Entry::getKey).forEach(System.out::println);
     }
 
-    private static String getInputData(PrintStream printStream, InputStream inputStream) {
-        printStream.print("Введите строку: ");
-        String result = new Scanner(inputStream).nextLine();
+    private static String getInputData() {
+        System.out.print("Введите строку: ");
+        String result = new Scanner(System.in).nextLine();
         if (result.isEmpty()) {
             return "Семь раз отмерь, 1 раз   отрежь!";
         }
