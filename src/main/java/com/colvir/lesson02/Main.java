cad8d743fbc3946b705dde822a4af8451ddfba67
sdoroshin <sdoroshin@colvir.ru>
2025-04-11 09:31:34 +0300

    BootCampJ5_01 initial

--- src/main/java/com/colvir/lesson02/Main.java
+++ src/main/java/com/colvir/lesson02/Main.java
@@ -1,17 +1,16 @@
 package com.colvir.lesson02;
 
 import com.colvir.lesson02.components.TemperatureConverterServiceImpl;
-import org.springframework.context.ApplicationContext;
+import com.colvir.lesson02.services.TemperatureConverterService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext("com.colvir.lesson02");
-        applicationContext.getBean(TemperatureConverterServiceImpl.class)
-                .convert("CelsiusToFahrenheit", 10);
-        applicationContext.getBean(TemperatureConverterServiceImpl.class)
-                .convert("CelsiusToKelvin", 10);
+        TemperatureConverterService temperatureConverterService =
+                new AnnotationConfigApplicationContext("com.colvir.lesson02")
+                .getBean(TemperatureConverterServiceImpl.class);
 
+        temperatureConverterService.convert("CelsiusToFahrenheit", 10);
+        temperatureConverterService.convert("CelsiusToKelvin", 10);
     }
 }
