52b8a2a02af4355521f38054789f320b94d35319
sdoroshin <sdoroshin@colvir.ru>
2025-04-11 09:07:47 +0300

    BootCampJ5_02 initial

--- src/main/java/com/colvir/lesson02/components/TemperatureConverterServiceImpl.java
+++ src/main/java/com/colvir/lesson02/components/TemperatureConverterServiceImpl.java
@@ -2,21 +2,25 @@ package com.colvir.lesson02.components;
 
 import com.colvir.lesson02.services.TemperatureConverter;
 import com.colvir.lesson02.services.TemperatureConverterService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TemperatureConverterServiceImpl implements TemperatureConverterService {
-    @Autowired
+public class TemperatureConverterServiceImpl implements TemperatureConverterService, ApplicationContextAware {
     private ApplicationContext applicationContext;
 
     @Override
     public void convert(String type, float sourceTemperature) {
-
         TemperatureConverter temperatureConverter = applicationContext.getBean(type, TemperatureConverter.class);
         System.out.printf("Результат перевода %s температуры %.2f%s = %.2f%s %n", type, sourceTemperature,
                 temperatureConverter.getSourceTemperatureTicket(),
                 temperatureConverter.convert(sourceTemperature), temperatureConverter.getDestTemperatureTicket());
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
     }
 }
